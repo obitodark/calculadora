4a0a2ef3e308d8068a7f8b7f711a5cfbe03465d0
obitodark <mayhixccallo@gmail.com>
2023-06-01 15:31:53 -0500

    se corrige la ruta por defacto

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../page/home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
